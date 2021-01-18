8f8208cb4710b214acbf9d073259951852a5a0e1
Matthew Skinner <matt@pcmus.com>
2021-01-18 22:53:52 +1100

    Misc Improvements and log cleanup.
    Signed-off-by: Matthew Skinner <matt@pcmus.com>

--- bundles/org.openhab.binding.opensprinkler/src/main/java/org/openhab/binding/opensprinkler/internal/util/Parse.java
+++ bundles/org.openhab.binding.opensprinkler/src/main/java/org/openhab/binding/opensprinkler/internal/util/Parse.java
@@ -42,7 +42,11 @@ public class Parse {
     public static int jsonInt(String jsonData, String keyName) {
         JsonElement jelement = jsonParser.parse(jsonData);
         JsonObject jobject = jelement.getAsJsonObject();
-        return jobject.get(keyName).getAsInt();
+        jelement = jobject.get(keyName);
+        if (jelement == null) {
+            return 0;// prevents a NPE if the key does not exist.
+        }
+        return jelement.getAsInt();
     }
 
     /**
